66d3a6e0d5ffed665d2778473267e8f132104035
hainguyenvan2703 <hainguyenvan2703@gmail.com>
2025-05-18 23:02:06 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import path from "path";
+
+export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/f8-zoom-module-1" : "/",
+  root: "src",
+  publicDir: "../public",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "src/index.html"),
+        about: path.resolve(__dirname, "src/about.html"),
+        blog: path.resolve(__dirname, "src/blog.html"),
+        contact: path.resolve(__dirname, "src/contact.html"),
+        practice: path.resolve(__dirname, "src/practice.html"),
+        // ...
+      },
+    },
+  },
+  plugins: [ViteEjsPlugin()],
+});
